45bf34abe519383159ace8c3631136bc28894cf7
Lucianfengwu <ljf@computer.local>
2016-12-04 13:17:34 +0800

    init project

---
+++ app/src/main/java/com/ljf/musicicon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ljf.musicicon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
